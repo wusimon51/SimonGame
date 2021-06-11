572568c83f03d065b1d174663b156d09b081247c
wusimon51 <wusimon51@gmail.com>
2021-06-10 21:45:02 -0400

    add styling to color buttons

--- Main.py
+++ Main.py
@@ -13,6 +13,9 @@ def press_yellow():
 def press_blue():
     print('blue')
 
+def start():
+    print('start')
+
 root = tk.Tk()
 root.title('Simon Game')
 
@@ -24,16 +27,21 @@ root.rowconfigure(0, weight=1)
 score = tk.Label(mainframe, text='Score: ')
 score.grid(column=0, row=1, sticky=('W'))
 
-green_button = tk.Button(mainframe, text='', command=press_green, bg='green')
+start_button = tk.Button(mainframe, text='Start', command=start)
+start_button.grid(column=1, row=1, sticky='E')
+
+pixel = tk.PhotoImage(width=1, height=1)
+
+green_button = tk.Button(mainframe, text='', image=pixel, width=150, height=150, command=press_green, bg='#30d94c')
 green_button.grid(column=0, row=2, sticky='W')
 
-red_button = tk.Button(mainframe, text='', command=press_red, bg='red')
+red_button = tk.Button(mainframe, text='', image=pixel, width=150, height=150, command=press_red, bg='#e92539')
 red_button.grid(column=1, row=2, sticky='E')
 
-yellow_button = tk.Button(mainframe, text='', command=press_yellow, bg='yellow')
+yellow_button = tk.Button(mainframe, text='', image=pixel, width=150, height=150, command=press_yellow, bg='#f8f120')
 yellow_button.grid(column=0, row=3, sticky='W')
 
-blue_button = tk.Button(mainframe, text='', command=press_blue, bg='blue')
+blue_button = tk.Button(mainframe, text='', image=pixel, width=150, height=150, command=press_blue, bg='#1470d2')
 blue_button.grid(column=1, row=3, sticky='E')
 
 root.mainloop()
